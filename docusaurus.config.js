1e37c01517cdff343ad6f73cb852eadf6399bcca
HangFamily <hangquoctai.family@gmail.com>
2023-02-23 15:32:36 +0700

    Cloudflare deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,11 +10,11 @@ const config = {
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'http://f1genz.com/',
+  // Set the production url of your site here 
+  url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
